2237d32670b0e24a932afa43512aee86d820b2ca
Sofia <sofikurochkina@gmail.com>
2023-04-27 23:23:13 +0300

    Time: 36 ms (93.87%), Space: 14.3 MB (50.84%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        dict = {}
+        
+        for index,num in enumerate(nums):
+            if num in dict:
+                return [index,dict[num]]
+            else:
+                dict[target-num] = index
+        return []
+
+
+        
