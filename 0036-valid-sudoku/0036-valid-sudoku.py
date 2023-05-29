96a357aac312742d5c60b3376f685347557324ae
Sofia <sofikurochkina@gmail.com>
2023-05-29 20:50:08 +0200

    Time: 80 ms (30.64%), Space: 13.4 MB (58.82%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -7,21 +7,22 @@ class Solution(object):
                 for col in range(len(board)):
                     if board[row][col]!='.':
                         arr.append(board[row][col])
-                if len(arr) != len(set(arr)):
+                if len(arr)!=len(set(arr)):
                     return False
-                    
+            
             return True
-        
+                
         def checking_column(board):
             for col in range(len(board)):
                 arr = []
                 for row in range(len(board)):
                     if board[row][col]!='.':
                         arr.append(board[row][col])
-                if len(arr) != len(set(arr)):
+                if len(arr)!=len(set(arr)):
                     return False
-            
+                
             return True
+
         
         def checking_sub_box(board):
             for row in (0,3,6):
@@ -29,13 +30,13 @@ class Solution(object):
                     arr = []
                     for r in range(row,row+3):
                         for c in range(col,col+3):
-                            if board[r][c] !='.':
+                            if board[r][c]!='.':
                                 arr.append(board[r][c])
-                    if len(arr) != len(set(arr)):
+                    if len(arr)!=len(set(arr)):
                         return False
                     
-            return True 
-        
+            return True
+            
         return checking_row(board) and checking_column(board) and checking_sub_box(board)
         
         
